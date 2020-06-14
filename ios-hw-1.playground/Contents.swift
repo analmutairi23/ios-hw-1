ddaf2c5bbd1bc862f36ecc02a3bbbdfdca61a904
analmutairi23 <anoudmutairi24@gmail.com>
2020-06-14 12:09:49 +0300

    done

--- ios-hw-1.playground/Contents.swift
+++ ios-hw-1.playground/Contents.swift
@@ -7,15 +7,15 @@
 
 let name = "Ahmad"
 
-name = "Salem"
+let name1 = "Salem"
 
 
 var myWeight = 60
-var FriendWeight = 80.5
-var height = 1.5
+var FriendWeight = 80
+var height = 2
 
 
-var weightSum = myWeight + FriendWeight
+var weightSum = (myWeight + FriendWeight)
 
 
 
@@ -38,35 +38,37 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = "Anoud"
+var lastName1 = "Almutairi"
+var email1 = "anoudmutairi23@gmail.com"
+var phoneNumber1 = "96690860"
+var age1 = 15
+var country1 = "kuwait"
+var area1 = "yarmouk"
+var password = "student445"
+var isKuwaiti = true
 
 //Person 2
-
-
-
-
-
-
-
-
-
+var firstName2 = "jood"
+var lastName2 = "sultan"
+var email2 = "joodsultan04@gmail.com"
+var phoneNumber2 = "97897435"
+var age2 = 19
+var country2 = "kuwait"
+var area2 = "sabah alsalem"
+var password2 = "student101"
+var isKuwaiti2 = true
 
 //Person 3
-
-
-
-
-
-
-
+var firstName3 = "sham"
+var lastName3 = "hussain"
+var email3 = "shamhussain03"
+var phoneNumber3 = "45329085"
+var age3 = 16
+var country3 = "kuwait"
+var area3 = "salmiya"
+var password3 = "student333"
+var isKuwaiti3 = false
 
 
 /*
@@ -80,5 +82,64 @@ var isKuwaiti = false
  * قارن اذا عمر الاشخاص يساوي  ١٧
  * قارن اذا عمر الاشخاص لا يساوي  ١٧
 
+
  
  */
+//name
+firstName1 == firstName2
+firstName1 != firstName2
+firstName1 == firstName3
+firstName1 != firstName3
+firstName2 == firstName1
+firstName2 != firstName1
+firstName2 == firstName3
+firstName2 != firstName3
+firstName3 == firstName1
+firstName3 != firstName1
+firstName3 == firstName2
+firstName3 != firstName2
+
+//age
+age1==age2
+age1==age3
+age2==age1
+age2==age3
+age3==age1
+age3==age2
+
+//country
+country1==country2
+country1==country3
+country2==country1
+country2==country3
+country3==country1
+country3==country2
+
+//age 2.0
+
+     //person 1
+18 > age1
+18 < age1
+17 == age1
+17 != age1
+
+
+    //person2
+18 > age2
+18 < age2
+17 == age2
+17 != age3
+
+    //person3
+18 > age3
+18 < age3
+17 == age3
+17 != age3
+
+
+
+
+
+
+
+
